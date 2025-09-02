15ec4eb4c77a565a357954a02e06e40e94d542c6
Brian <596833+flizzer@users.noreply.github.com>
2025-09-02 00:11:28 -0400

    exposing CurrentVelocity for Player Animation Blueprint

--- Source/ActionCombat/Public/Animations/PlayerAnimInstance.h
+++ Source/ActionCombat/Public/Animations/PlayerAnimInstance.h
@@ -13,5 +13,10 @@ UCLASS()
 class ACTIONCOMBAT_API UPlayerAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+protected:
+	//the {} indicates that we want explicit type conversion and that
+	//an error will throw if we try to assign the wrong type value -- bhd
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animation)
+	float CurrentVelocity{ 0.0f };
 };
